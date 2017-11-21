465ec48259da93deb37fbafabc434015a3b34207
k <theoneas1@gmail.com>
2017-11-20 22:48:28 -0500

    first section works

--- EntityExtraction.py
+++ EntityExtraction.py
@@ -13,7 +13,7 @@ def preprocess_complaint_narratives(df): # receives argument df. Is this datafra
 		#word tokenize
 		sent = [nltk.______(sent) for s in sent] # TURNS SENTENCES INTO WORDS -> word_tokenize()
 		#Part of speech tagging
-		sent = [nltk.______(sent) for s in sent] # NOT SURE ABOUT THIS SECTION
+		sent = [nltk.______(sent) for s in sent] # NOT SURE ABOUT THIS SECTION -> pos_tag()
 		for s in sent: # RUNS LOOP FOR ALL ITEMS IN SENT
 			sentences.append(s) # APPENDS ALL THE ITEMS INTO SENTENCES
 
@@ -27,12 +27,15 @@ banks = set(df['______']) # Company
 tokens = preprocess_complaint_narratives(________) # compNarr
 tree = [nltk.chunk.ne_chunk(t) for t in tokens]
 
-def extract_entity_names(t):
-    entity_names = []
+# I NEED TO RUN THE CODE UP TO THIS POINT TO SEE WHAT KIND OF LABELS I CAN HAVE HERE
+# I NEED TO SEE THE ELEMENTS OF THE TREE
+def extract_entity_names(t): # A ELEMENT OF THE TREE IS SENT TO THIS FUNCTION
+    entity_names = [] # CREATES AN EMPTY LIST TO STORE ENTITIES
     if hasattr(t, 'label') and t.label():
-        if t.label() == '_________':
-            entity_names.append(' '.join([child[0] for child in t])) # something missing
-        else:
+    # hasattr: The arguments are an object and a string. The result is True if the string is the name of one of the object’s attributes, False if not.
+        if t.label() == '_________': # MAYBE THIS LABEL IS SUPPOSED TO BE ORGANIZATION OR SOMETHING THAT REFLECT THE ENTITY AS AN ORGANIZATION, OR MAYBE EVEN AN ELEMENT FROM A DICTIONARY, LIKE THE LIST OF BANKS
+            entity_names.append(' '.join([child[0] for child in t])) # something missing # CHILD IS A LOOP COUNTER FOR ELEMENTS IN t, THIS LOOP IS TAKING THE FIRST ELEMENT IN THE LOOP
+        else: # THIS IS A RECURSIVE IF!!! IF THE PREVIOUS CONDITION FAILS THEN ALL THE ELEMENTS IN THE T ARE SENT TO THIS SAME FUNCTION AND APPENDED TO THE LIST
             for child in t:
                 entity_names.extend(extract_entity_names(child))
 	return entity_names
